60f95090973013f1df7ea87850749ebaa0be2534
Hizkia Steven <xvii.hs@gmail.com>
2020-03-23 03:46:12 +0700

    Perbaikan untuk kriteria 4.1.1 poin 2

--- www/application/views/EntriJadwalDosen/main.php
+++ www/application/views/EntriJadwalDosen/main.php
@@ -4,10 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html class="no-js" lang="en">
     <?php $this->load->view('templates/script_foundation'); ?>
     <?php $this->load->view('templates/head_loggedin'); ?>
-	<?php $this->load->view('templates/flashmessage'); ?>
     <?php $this->load->helper('url'); ?>
     <body>
         <?php $this->load->view('templates/topbar_loggedin'); ?>
+        <?php $this->load->view('templates/flashmessage'); ?>
 
         <div class="row">
 
